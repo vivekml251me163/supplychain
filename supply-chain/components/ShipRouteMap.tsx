01b10c96ec8d465f033fee1fe4e0e9b4bca265fc
vivekml251me163 <vivekml2007@gmail.com>
2026-04-05 01:12:27 +0530

    manager/ship5

--- supply-chain/components/ShipRouteMap.tsx
+++ supply-chain/components/ShipRouteMap.tsx
@@ -2,6 +2,17 @@
 
 import { MapContainer, TileLayer, Polyline, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+
+// Fix for default marker icons in react-leaflet
+if (typeof window !== 'undefined') {
+  delete (L.Icon.Default.prototype as any)._getIconUrl
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
+  })
+}
 
 interface RoutePoint {
   lat: number
